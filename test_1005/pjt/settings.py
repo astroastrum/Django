7983eaa507342024468f2741e8e1b4a7dc4692a9
JungAh Choi <108647883+astroastrum@users.noreply.github.com>
2022-10-05 17:31:13 +0900

    Static

--- test_1005/pjt/settings.py
+++ test_1005/pjt/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# 정적 파일에 대한 설정
 STATIC_URL = '/static/'
 
 # Default primary key field type
