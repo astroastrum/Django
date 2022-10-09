ed8850370cbb69d5935e4df2be12eb15be125681
JungAh Choi <108647883+astroastrum@users.noreply.github.com>
2022-10-10 02:26:16 +0900

    random

---
+++ test_1010/articles/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+import random
+
+
+
+# Create your views here.
+'''
+def index(request):
+  context = {
+    'name': 'october',
+    'img': 'https://thumbs.dreamstime.com/b/welcome-sign-flower-blooming-decoration-pink-background-175919766.jpg',
+  }
+  return render(request, 'index.html', context)
+'''
+
+
+'''
+def index(request):
+  names = ['october', 'november', 'december']
+  name = random.choice(names)
+  context = {
+    'name': name,
+    'img': 'https://thumbs.dreamstime.com/b/welcome-sign-flower-blooming-decoration-pink-background-175919766.jpg',
+  }
+  return render(request, 'index.html', context)
+'''
+
+'''
+# 사용자들이 랜덤하게 입력할 값을 다룰 수 있는 변수 = name
+# 사용자들의 입력값 = name
+def welcome(request, name):
+  # 사람들이 /welcome/본인이름을 입력하면, 환영 인사와 이름을 보여줌
+  context = {
+    'name': name,
+    'drinks': [
+      'coke', 'latte', 'macchiato'
+    ],
+
+    'images': [
+      'https://thumbs.dreamstime.com/b/welcome-sign-flower-blooming-decoration-pink-background-175919766.jpg',
+      'https://thumbs.dreamstime.com/b/welcome-sign-tulip-flower-blooming-decoration-pink-background-182818281.jpg',
+      'https://thumbs.dreamstime.com/b/welcome-sign-flower-blooming-decoration-wooden-background-221043138.jpg',
+    ],
+  }
+  return render(request, 'welcome.html', context)
+
+'''
---
+++ test_1010/pjt/urls.py
@@ -0,0 +1,27 @@
+"""pjt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from articles import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    # 사용자가 입력한 값을 활용해야함
+    # 값을 어떤 이름으로 활용해야 하는지 작성
+    # 사용자는 사용자의 이름을 작성한다
+    path('welcome/<name>/', views.welcome),
+]
