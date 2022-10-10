0322d5128e1db3a92153b0e2d8f62194b26177d0
JungAh Choi <108647883+astroastrum@users.noreply.github.com>
2022-10-10 22:48:38 +0900

    CRUD to practice daily

--- tc_1010/posts/urls.py
+++ tc_1010/posts/urls.py
@@ -16,4 +16,11 @@ urlpatterns = [
   path('create/', views.create, name='create'),
   # pk의 자료형은 int
   path('delete/<int:pk>', views.delete, name='delete'),
+  # 요청과 응답을 항상 생각
+  # 어떤 주소로 요청하면
+  # 어떤 VIEW 함수를 응답할까
+  # path에서 명시한 동적인자의 이름과 VIEW함수에서 전달받은 인자가 동일해야한다.
+  path('detail/<int:pk_>', views.detail, name='detail'),
+  path('edit/<int:pk_>', views.edit, name='edit'),
+  path('update/<int:pk_>', views.update, name='update'),
 ]
--- tc_1010/posts/views.py
+++ tc_1010/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Post
 
 # Create your views here.
+# 첫번째 read는 index
 def index(request):
   # 모든 글 목록을 보여준다
   # 1. DB에서 모든 글을 불러온다
@@ -12,9 +13,35 @@ def index(request):
   }
   return render(request, 'posts/index.html', context)
 
+
+# 두번째 read는 하나의 데이터에 대한 정보를 출력
+def detail(request, pk_):
+  # 17번 글이면 17번 글을 불러와서 detail page에 전달해야한다
+  # get() 메소드를 사용해서 특정 pk의 데이터를 불러온다
+  # pk를 index.html에서 가져와야한다
+  # 불러온 데이터를 변수에 할당
+  post = Post.objects.get(pk = pk_)
+  # template에서 이 값들을 사용하기 위해 render에 인자로 넘겨준다
+  context = {
+    'post': post,
+  }    
+  return render(request, 'posts/detail.html', context)
+
+
 # posts/new.html에 posts를 반드시 작성해야함
 def new(request):
   return render(request, 'posts/new.html')
+
+
+def edit(request, pk_):
+  # get 메소드 사용해서 특정 pk를 불러온다
+  # read
+  post = Post.objects.get(pk = pk_)
+  context = {
+    "post": post,
+  }
+  return render(request, 'posts/edit.html', context)
+
 
 def create(request):
   # DB에 저장
@@ -34,6 +61,22 @@ def create(request):
   }
   # return render(request, 'posts/create.html', context)
   return redirect('posts:index')
+
+
+def update(request, pk_):
+  # update할 특정 데이터를 가져온다
+  post = Post.objects.get(pk = pk_)
+  title_ = request.GET.get('title')
+  content_ = request.GET.get('content')
+
+  # 데이터를 수정
+  post.title = title_ 
+  post.content = content_
+  # 데이터를 수정한 것을 반영
+  post.save()
+  # 데이터의 detail 페이지로 redirect
+  return redirect('posts:detail', post.pk)
+
 
 
 def delete(request, pk):
