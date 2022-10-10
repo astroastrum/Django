593a9467efc79a4d0c7c45217268416500db843a
JungAh Choi <108647883+astroastrum@users.noreply.github.com>
2022-10-10 17:14:12 +0900

    namespace 생성

--- tc_1010/articles/urls.py
+++ tc_1010/articles/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+# 동일한 index가 존재(practices에)할 경우 어떤 앱의 index(url 변수명)인지 구별할 수 있다
+app_name = 'articles'
+
 urlpatterns = [
-  path('', views.index),
-  path('create/', views.create),
+  # 링크를 부를때 index라고 부름
+  # name은 url을 변수화한것 = 여러변 활용할 때 변수를 사용
+  path('', views.index, name='index'),
+  path('create/', views.create, name='create'),
 ]
