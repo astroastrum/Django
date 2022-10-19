bc5cd1e2fd4e1ac58f27e44c4f0a2316fd804a6b
JungAh Choi <jungahchoi92@gmail.com>
2022-10-19 15:28:34 +0900

    CRUD full explained

--- test_1019/articles/urls.py
+++ test_1019/articles/urls.py
@@ -5,8 +5,14 @@ app_name = 'articles'
 
 urlpatterns = [
   path('', views.index, name='index'),
+  # new page: 글 작성/제출
   path('new/', views.new, name='new'),
+  # create page: 작성한 글을 db에 저장
   path('create/', views.create, name='create'),
+  # detail page: 작성한 글을 보여주는 목록
   path('<int:pk>', views.detail, name='detail'),
+  path('<int:pk>/delete/', views.delete, name='delete'),
+  path('<int:pk>/edit/', views.edit, name='edit'),
+  path('<int:pk>/update/', views.update, name='update'),
 
 ]
--- test_1019/articles/views.py
+++ test_1019/articles/views.py
@@ -23,8 +23,12 @@ def create(request):
   content = request.POST.get('content')
   # DB에 저장
   # 2. 데이터가 저장됨
-  Article.objects.create(title=title, content=content)
-  return redirect('articles:index')
+  article = Article.objects.create(title=title, content=content)
+  
+  return redirect('articles:detail', article.pk)
+  
+  # article.pk 오류
+  # return redirect('articles:detail', article.pk)
   
 
 
@@ -35,3 +39,39 @@ def detail(request, pk):
     'article': article,
   }
   return render(request, 'articles/detail.html', context)
+
+
+# detail 페이지에 작성
+def delete(request, pk):
+  # 특정 pk의 데이터를 불러온다
+  article = Article.objects.get(pk=pk)
+  article.delete()
+  return redirect('articles:index')
+
+
+# detail 페이지에 edit 페이지로 이동하는 링크를 작성한다
+# edit 페이지로 이동하면 사용자가 다시 글을 작성할 수 있는 form이 주어진다
+# 작성한 글을 제출하면 update에서 db에 저장한다
+# db에 저장되면 실제로 edit한 글이 update된다
+def edit(request, pk):
+  article = Article.objects.get(pk=pk)
+  context = {
+    'article': article,
+  }
+  return render(request, 'articles/edit.html', context)
+
+
+# edit.html에 <form action="{% url 'articles:update' article.pk %}" method="POST">
+def update(request, pk):
+  article = Article.objects.get(pk=pk)
+  article.title = request.POST.get('title')
+  article.content = request.POST.get('content')
+  article.save()
+  # update을 하면 detail 페이지로 이동
+  return redirect('articles:detail', article.pk)
+
+
+
+
+
+
