d4097a5faf55c0abebcef456e850b49f04bbcf87
JungAh Choi <jungahchoi92@gmail.com>
2022-10-23 23:23:54 +0900

    articles/create

---
+++ test_1022/articles/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Article
+
+# ModelForm class 상속받음
+class ArticleForm(forms.ModelForm):
+  class Meta:
+    # 어떤 모델을 기반으로 form을 만들것인가
+    model = Article
+    fields = ['title', 'content']
--- test_1022/articles/views.py
+++ test_1022/articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,17 @@ def index(request):
     'articles': articles
   }
   return render(request, 'articles/index.html', context)
+
+
+def create(request):
+  if request.method == 'POST':
+    article_form = ArticleForm(request.POST)
+    if article_form.is_valid():
+      article_form.save()
+      return redirect('articles:create')
+    else:
+      article_form = ArticleForm()
+    context = {
+      'article_form': article_form
+    }
+    return render(request, 'articles/index.html', context)
