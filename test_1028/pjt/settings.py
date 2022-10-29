6780f54c4a7993c6dd18a3daf82d7f91e4dddfa3
JungAh Choi <jungahchoi92@gmail.com>
2022-10-29 23:03:39 +0900

    reviews/reviews

--- test_1028/pjt/settings.py
+++ test_1028/pjt/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reviews',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- test_1028/reviews/views.py
+++ test_1028/reviews/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, 'reviews/index.html')
+
+def reviews(request):
+  return render(request, 'reviews/reviews.html')
