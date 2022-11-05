5c2e3930062e7c8c46fd8cb3fa8789c2d6d896eb
JungAh Choi <jungahchoi92@gmail.com>
2022-11-05 19:24:35 +0900

    accounts/working

--- test_1028/accounts/views.py
+++ test_1028/accounts/views.py
@@ -12,7 +12,21 @@ from django.contrib.auth.decorators import login_required
 app_name = 'accounts'
 # Create your views here.
 def signup(request):
-  return render(request, 'accounts/signup.html')
+    if request.method == "POST":
+        forms = SignupForm(request.POST, request.FILES)
+        if forms.is_valid():
+            forms.save()
+            return redirect("reviews:index")
+    else:
+        forms = SignupForm()
+    context = {
+        "forms": forms,
+    }
+    return render(request, "accounts/signup.html", context)
+
+def index(request):
+    members = get_user_model().objects.all()
+    return render(request, "accounts/index.html")
 
 def login(request):
     # 사용자가 입력한 데이터 가져오기
@@ -27,3 +41,86 @@ def login(request):
         form = AuthenticationForm()
     context = {"forms": form}
     return render(request, "accounts/login.html", context)
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:index")
+
+
+@login_required
+def update(request):
+    if request.method == "POST":
+        forms = CustomUserChangeForm(request.POST, instance=request.user)
+        if forms.is_valid():
+            forms.save()
+            return redirect("accounts:detail", request.user.pk)
+    else:
+        forms = CustomUserChangeForm(instance=request.user)
+    context = {
+        "forms": forms,
+    }
+    return render(request, "accounts/update.html", context)
+
+
+@login_required
+def change_password(request):
+    if request.method == "POST":
+        forms = PasswordChangeForm(request.user, request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect("accounts:detail")
+    else:
+        forms = PasswordChangeForm(request.user)
+    context = {
+        "forms": forms,
+    }
+    return render(request, "accounts/change_password.html", context)
+
+
+def detail(request, user_pk):
+    person = get_user_model()
+    person = get_object_or_404(person, pk=user_pk)
+    context = {
+        "person": person,
+    }
+    return render(request, "accounts/detail.html", context)
+
+
+@login_required
+def follow(request, user_pk):
+    person = get_user_model().objects.get(pk=user_pk)
+    if person != request.user:
+        if person.followers.filter(pk=request.user.pk).exists():
+            person.followers.remove(request.user)
+        else:
+            person.followers.add(request.user)
+        return redirect("accounts:detail", user_pk)
+    else:
+        return HttpResponseForbidden()
+
+
+@login_required
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect("accounts:login")
+
+
+@login_required
+def profile_update(request):
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:detail", request.user.pk)
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(
+        request,
+        "accounts/profile_update.html",
+        {
+            "form": form,
+        },
+    )
