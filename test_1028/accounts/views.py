88759f11aef27328e77f398980a478e36a98c723
JungAh Choi <jungahchoi92@gmail.com>
2022-11-05 19:25:30 +0900

    accounts/working

--- test_1028/accounts/views.py
+++ test_1028/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import SignupForm
 from django.contrib.auth import login as auth_login
