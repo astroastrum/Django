a1f9799708c6bdd05f99073182d544075826ea2a
JungAh Choi <jungahchoi92@gmail.com>
2022-11-13 15:54:16 +0900

    chore: image upload

--- test_1112/articles/models.py
+++ test_1112/articles/models.py
@@ -8,11 +8,15 @@ from django.conf import settings
 
 
 class Article(models.Model):
-  title = models.CharField(max_length=20)
-  content = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  image = models.ImageField(upload_to='images/', blank=True)
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # 이미지 원본 필요 없으면 ProcessedImageField를 사용해서 이미지 사이즈 조절 가능
+    image = ProcessedImageField(upload_to='images/', blank=True,
+                                processors=[ResizeToFill(1200, 800)],
+                                format='JPEG',
+                                options={'quality': 80})
 
 class Comment(models.Model):
     content = models.TextField()
