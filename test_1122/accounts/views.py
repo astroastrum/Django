8c31d13d3d58799bea181f77209092717fb6ab85
JungAh Choi <jungahchoi92@gmail.com>
2022-11-23 00:17:57 +0900

    feat: django-allauth

--- test_1122/accounts/views.py
+++ test_1122/accounts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def signup(request):
   return render(request, 'accounts/signup.html')
+
+# 로그인 기능 
+# django-allauth 사용하기
+# pip install django-allauth
