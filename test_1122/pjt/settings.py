c56fa5991d6640e3524a2fbec36c6f43271ed44d
JungAh Choi <jungahchoi92@gmail.com>
2022-11-27 17:03:15 +0900

    feat: blog app

--- test_1122/pjt/settings.py
+++ test_1122/pjt/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- test_1122/pjt/urls.py
+++ test_1122/pjt/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
 
 ]
