0315dceef371e8ae08b6774779eb19eeb5305799
JungAh Choi <jungahchoi92@gmail.com>
2022-11-27 17:06:49 +0900

    migrated Post Model

--- test_1122/blog/models.py
+++ test_1122/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField()
