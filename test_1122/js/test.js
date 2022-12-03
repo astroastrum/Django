6c0f374bcda6b56647d22a2d46835728bd005d44
JungAh Choi <jungahchoi92@gmail.com>
2022-12-03 23:35:10 +0900

    feat: true and false [array]

--- test_1122/js/test.js
+++ test_1122/js/test.js
@@ -6,4 +6,6 @@ const array1 = [6, 7, 8];
 const array2 = [6, 7, 8];
 console.log(array1 == array2); // 참조하는 array가 달라서 false
 
-
+const array6 = [6, 7, 8];
+const array7 = array6;
+console.log(array6 == array7); // 참조하는 array가 동일해서 true
