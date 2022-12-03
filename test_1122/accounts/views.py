e591e2a0fa2c5a287b80e5ceb4ef912dcb4ff4b9
JungAh Choi <jungahchoi92@gmail.com>
2022-12-03 23:32:47 +0900

    feat: JS files

--- test_1122/accounts/views.py
+++ test_1122/accounts/views.py
@@ -13,3 +13,29 @@ def signup(request):
 
 def login(request):
   return render(request, 'accounts/login.html')
+
+
+
+@login_required
+def detail(request, user_pk):
+    customer = get_user_model().objects.get(pk=user_pk)
+    review_pg = request.GET.get("reviewpage")
+    
+    customer_rv = customer.review_set.all()
+    # Paginator(분할될 객체, 페이지마다 넣을 객체수)
+    review_data = Paginator(customer_rv, 5)
+    review_page = review_data.get_page(review_pg)
+
+
+    #reviews = customer.review_set.order_by('-pk')
+    #review_page = request.GET.get('review_page', '1')
+    #review_paginator = Paginator(reviews, 5)
+    #review_page_obj = review_paginator.get_page(review_page)
+
+    context = {
+        "customer": customer,
+        "review_page": review_page,
+        # "reviews": review_page_obj,
+
+    }
+    return render(request, "accounts/detail.html", context)
---
+++ test_1122/js/test.js
@@ -0,0 +1,9 @@
+console.log('블루베리' == '블루베리'); // true
+console.log(1000 < 6000); // true
+console.log(1000 > 6000); // false
+
+const array1 = [6, 7, 8];
+const array2 = [6, 7, 8];
+console.log(array1 == array2); // 참조하는 array가 달라서 false
+
+
