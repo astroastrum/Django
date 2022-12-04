80b72d8e72fd022350952488a45a660335589384
JungAh Choi <jungahchoi92@gmail.com>
2022-12-04 22:26:36 +0900

    feat: style for JavaScript added

---
+++ test_1122/js/style.js
@@ -0,0 +1,11 @@
+const box = document.querySelector('#box');
+// 배경을 초록색으로 변경 
+box.style.backgroundColor = 'green';
+
+// HTML
+// <p id="december">Christmas in South Korea</p>
+const december = document.querySelector('#december');
+// color 속성 변경
+december.style.color = 'white';
+// font-size 속성 변경
+december.style.fontsize = '10px';
