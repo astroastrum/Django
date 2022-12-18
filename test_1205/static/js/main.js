c04e9d5e511d45ca638888846b869abbe9827904
JungAh Choi <jungahchoi92@gmail.com>
2022-12-18 23:18:20 +0900

    image loading

--- test_1205/static/js/main.js
+++ test_1205/static/js/main.js
@@ -5,3 +5,11 @@ const seconds = date.getSeconds();
 
 const label = '${hour}시${minutes}분${seconds}초';
 
+// 이미지 로딩
+const imgJ = document.querySelector
+('#myImageJ');
+imgA.src = 'images/photo_j.jpg';
+
+const imgK = document.querySelector
+('#myImageK');
+imgB.src = 'images/photo_k.jpg';
