6cf0fc8548aa7744dfd632ed75018c44fc579a0f
JungAh Choi <jungahchoi92@gmail.com>
2022-12-27 22:11:28 +0900

    feat: time added

--- test_1205/static/js/main.js
+++ test_1205/static/js/main.js
@@ -62,6 +62,15 @@ window.addEventListener('scroll', () => {
   console.log('스크롤', window.scrollX, window.scrollY);
 });
 
+// time
+const date = new Date();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+
+const label = '${hour}시${minutes}분${seconds}초';
+
+// time
 const date = new Date();
 const hour = date.getHours();
 const minutes = date.getMinutes();
